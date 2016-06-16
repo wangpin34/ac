1ffb298a4218dc440a493fa679bc38800e4ea293
wangpin34 <270280522@qq.com>
2016-06-16 13:39:35 +0800

    Support copy to

--- src/components/OutBox.vue
+++ src/components/OutBox.vue
@@ -1,18 +1,19 @@
 <template>
-	<section name="outbox">
-		<span class="title" :class="{'valid': valid}"># {{index}}</span>
-		<json-root v-if="valid" :json="obj"></json-root>
-		<div v-else>
-		Error happended
-		</div>
-	</section>
+    <section name="outbox">
+        <span class="title" :class="{'valid': valid}"># {{index}}</span>
+        <button class="copyBtn" :id.once="id" :data-clipboard-text="copyText">Copy</button>
+        <json-root v-if="valid" :json="obj"></json-root>
+        <div v-else>
+        Error happended
+        </div>
+    </section>
 </template>
 
 <style lang="sass">
 [name="outbox"] {
-	background: #CCDAD9;
+    background: #CCDAD9;
 
-	width: 440px;
+    width: 440px;
     min-height: 100px;
     padding: 12px 30px;
     text-align: left;
@@ -23,14 +24,18 @@
     border-radius: .5rem;
 
     .title {
-    	width: 440px;
-    	display: inline-block;
-    	margin-bottom: 2rem;
-    	color: #ffffff;
-    	background: red;
-    	&.valid {
-    		background: green;
-    	}
+        width: 440px;
+        display: inline-block;
+        margin-bottom: 2rem;
+        color: #ffffff;
+        background: red;
+        &.valid {
+            background: green;
+        }
+    }
+
+    .copyBtn {
+        float:right;
     }
 }
 </style>
@@ -38,17 +43,34 @@
 <script>
 import JsonRoot from './JsonRoot.vue'
 export default {
-	props: ['index','json'],
-	data() {
-		let json = this.json
-		return {
-			obj: json.obj,
-			err: json.err,
-			valid: json.valid
-		}
-	},
-	components: {
-		JsonRoot
-	}
+    props: ['index','json'],
+    data() {
+        let id = Math.random() + ''
+        let json = this.json
+
+        return {
+            id: id,
+            copyText: JSON.stringify(json.obj, null, 4),
+            obj: json.obj,
+            err: json.err,
+            valid: json.valid
+        }
+    },
+    ready() {
+        let client = new ZeroClipboard( document.getElementById(this.id) )
+        client.on( 'ready', readyEvent => {
+
+          client.on( 'aftercopy', event =>  {
+
+            //alert("Copied text to clipboard: " + event.data["text/plain"] );
+
+            alert('Copy succeed !')
+
+          });
+        });
+    },
+    components: {
+        JsonRoot
+    }
 }
 </script>
